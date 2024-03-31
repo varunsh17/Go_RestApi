03c2779b3545bad92df9bbc2d8f33b212f7aaf4c
varun_sharma <varunsh170502@gmail.com>
2024-03-31 15:19:14 +0530

    Basic server starting

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading environment variables from environment variable list")
+	}
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
